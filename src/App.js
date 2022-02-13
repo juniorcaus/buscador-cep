dc80c704c2142287c3c64a2a5d9ada79e63dc759
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-02-12 23:09:14 -0300

    Criando projeto de buscar cep usando ReactJS

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu Cep..."
+        />
+
+        <button className="buttonSearche">
+          <FiSearch size={25} color="#000"/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 292929-025</h2>
+
+        <span>Rua: Canarinho barra</span>
+        <span>Complemento: Perto de um Hospital</span>
+        <span>Boa Vista</span>
+        <span>Vila Velha - ES</span>
+
+
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
